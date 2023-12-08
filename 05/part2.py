1dd6c89c2404de781153534898cb45edb6f380aa
Presley Graham <presley.graham@protonmail.com>
2023-12-08 14:25:24 -0500

    day 5 part 2 finally works

--- 05/part2.py
+++ 05/part2.py
@@ -10,76 +10,29 @@ class Range:
     start: int
     end: int
 
+    def intersect(self, other: Range) -> Range:
+        return Range(max(self.start, other.start), min(self.end, other.end))
 
-def compress(add_ranges: list[Range], remove_ranges: list[Range]) -> list[Range]:
-    """
-    Takes a list of possibly-overlapping ranges to add and remove,
-    and returns a list of non-overlapping ranges that cover these points.
-    """
-    # Adds should come before removals, so that two ranges that touch at one point
-    # are condensed into one range.
-    ADD, REMOVE = 0, 1
-    events: list[tuple[int, int]] = []
-    events.extend(e for r in add_ranges for e in [(r.start, ADD), (r.end, REMOVE)])
-    events.extend(e for r in remove_ranges for e in [(r.start, REMOVE), (r.end, ADD)])
-    events.sort()
-    out = []
-    left = 0
-    n = 0
-    for pos, what in events:
-        if what == ADD:
-            if n == 0:
-                left = pos
-            n += 1
-        elif what == REMOVE:
-            n -= 1
-            assert n >= 0
-            if n == 0:
-                out.append(Range(left, pos))
-    return out
+    @property
+    def empty(self):
+        return self.start >= self.end
 
 
+@attrs.define
 class RangeMap:
-    def __init__(self, ranges: dict[Range, int]) -> None:
-        self._dict = {0: 0}
-        for r, dest in ranges.items():
-            self._dict[r.start] = dest
-            self._dict.setdefault(r.end, r.end)
-        self._sorted_keys = sorted(self._dict.keys())
-
-    def _query_point(self, key: int) -> int:
-        nearest_key_idx = max(0, bisect.bisect(self._sorted_keys, key) - 1)
-        nearest_key = self._sorted_keys[nearest_key_idx]
-        return self._dict[nearest_key] + key - nearest_key
+    ranges: dict[Range, int]
 
     def _query_range(self, range_: Range) -> tuple[list[Range], list[Range]]:
-        add_list = []
-        remove_list = []
+        ranges = []
 
-        start_idx = max(0, bisect.bisect(self._sorted_keys, range_.start) - 1)
-        end_idx = bisect.bisect_left(self._sorted_keys, range_.end, lo=start_idx)
+        for r, diff in self.ranges.items():
+            if not (intersection := range_.intersect(r)).empty:
+                ranges.append(Range(intersection.start + diff, intersection.end + diff))
 
-        for i in range(start_idx, min(end_idx + 1, len(self._sorted_keys))):
-            start = max(range_.start, self._sorted_keys[i])
-            end = (
-                range_.end
-                if i == len(self._sorted_keys)
-                else min(range_.end, self._sorted_keys[i + 1])
-            )
-            if start >= end:
-                continue
-            dest = self._dict[self._sorted_keys[i]]
-            remove_list.append(Range(start, end))
-            add_list.append(Range(start + dest, end + dest))
-
-        return add_list, remove_list
+        return ranges
 
     def query(self, ranges: list[Range]) -> list[Range]:
-        results = [self._query_range(r) for r in ranges]
-        return compress(
-            [r for add_list, _ in results for r in add_list],
-            [r for _, remove_list in results for r in remove_list],
-        )
+        return [res for r in ranges for res in self._query_range(r)]
 
 
 def main():
@@ -100,7 +53,16 @@ def main():
         range_dict = {}
         for line in lines[1:]:
             dest_start, src_start, length = map(int, line.split())
-            range_dict[Range(src_start, src_start + length)] = dest_start
+            diff = dest_start - src_start
+            range_dict[Range(src_start, src_start + length)] = diff
+
+        # Fill gaps
+        sorted_ranges = sorted(range_dict.keys(), key=lambda x: x.start)
+        for r1, r2 in itertools.pairwise(sorted_ranges):
+            range_dict[Range(r1.end, r2.start)] = r1.end
+        range_dict[Range(0, sorted_ranges[0].start)] = 0
+        range_dict[Range(sorted_ranges[-1].end, 10**12)] = 0
+
         maps.append(RangeMap(range_dict))
 
     for m in maps:
