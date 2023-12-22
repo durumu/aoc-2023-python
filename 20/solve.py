835233fabeef6f410c724304b136fdef2ce54203
Presley Graham <presley.graham@protonmail.com>
2023-12-22 06:34:39 -0500

    day 20 part 1

--- 20/solve.py
+++ 20/solve.py
@@ -1,19 +1,31 @@
 from __future__ import annotations
 
+import itertools
 import sys
 from collections import defaultdict
 from collections.abc import Iterable
 from functools import cached_property
 
-from attrs import field, frozen
+from attrs import frozen
 
 
 @frozen
 class State:
-    on_flip_flops: set[str]
+    flip_flop_on: dict[str, bool]
     conjunction_memory: dict[str, set[str]]
+    rx_low: bool = False
     total_low: int = 0
     total_high: int = 0
+
+
+@frozen
+class Signal:
+    sender: str
+    receiver: str
+    is_high: bool
+
+    def __str__(self) -> str:
+        return f"{self.sender} -{'high' if self.is_high else 'low'}-> {self.receiver}"
 
 
 @frozen(slots=False)
@@ -31,54 +43,76 @@ class Machine:
         return parents
 
     def next_state(self, state: State) -> State:
-        receiving = {
-            node: ("broadcaster", False) for node in self.children["broadcaster"]
-        }
-        flip_flop_on = {node: node in state.on_flip_flops for node in self.flip_flops}
+        flip_flop_on = state.flip_flop_on.copy()
         conjunction_memory = {
             node: memory.copy() for node, memory in state.conjunction_memory.items()
         }
         lows_sent = highs_sent = 0
-        # print("button -low-> broadcaster")
+        rx_low = False
+        receiving = [Signal("button", "broadcaster", False)]
         while receiving:
-            sending = {}
-            for node, (sender, is_high) in receiving.items():
-                # print(sender, f"-{'high' if is_high else 'low'}->", node)
-                if is_high:
+            sending = []
+            for s in receiving:
+                if s.is_high:
                     highs_sent += 1
                 else:
                     lows_sent += 1
-                if node in self.flip_flops:
-                    if not is_high:
-                        flip_flop_on[node] ^= True
-                        for child in self.children[node]:
-                            sending[child] = (node, flip_flop_on[node])
-                elif node in self.conjunctions:
-                    if is_high:
-                        conjunction_memory[node].add(sender)
+                if s.receiver in self.flip_flops:
+                    if not s.is_high:
+                        flip_flop_on[s.receiver] ^= True
+                        for child in self.children[s.receiver]:
+                            sending.append(
+                                Signal(s.receiver, child, flip_flop_on[s.receiver])
+                            )
+                elif s.receiver in self.conjunctions:
+                    if s.is_high:
+                        conjunction_memory[s.receiver].add(s.sender)
                     else:
-                        conjunction_memory[node].discard(sender)
-                    send_high = len(conjunction_memory[node]) != len(self.parents[node])
-                    for child in self.children[node]:
-                        sending[child] = (node, send_high)
+                        conjunction_memory[s.receiver].discard(s.sender)
+                    send_high = len(conjunction_memory[s.receiver]) != len(
+                        self.parents[s.receiver]
+                    )
+                    for child in self.children[s.receiver]:
+                        sending.append(Signal(s.receiver, child, send_high))
                 else:
-                    for child in self.children[node]:
-                        sending[child] = (node, is_high)
+                    if not s.is_high and s.receiver == "rx":
+                        rx_low = True
+                    for child in self.children[s.receiver]:
+                        sending.append(Signal(s.receiver, child, s.is_high))
             receiving = sending
 
         return State(
-            {node for node, is_on in flip_flop_on.items() if is_on},
+            flip_flop_on,
             conjunction_memory,
-            state.total_low + lows_sent + 1,  # the button
+            rx_low,
+            state.total_low + lows_sent,
             state.total_high + highs_sent,
         )
 
     def count_pulses(self, runs: int) -> int:
         """returns (low pulses * high pulses)"""
-        state = State(set(), {node: set() for node in self.conjunctions})
+        state = State(
+            {node: False for node in self.flip_flops},
+            {node: set() for node in self.conjunctions},
+        )
         for _ in range(runs):
             state = self.next_state(state)
         return state.total_low * state.total_high
+
+    def presses_until_rx_low(self) -> int:
+        state = State(
+            {node: False for node in self.flip_flops},
+            {node: set() for node in self.conjunctions},
+        )
+        for i in itertools.count():
+            # print(i)
+            # bits = "".join(str(int(h)) for h in state.flip_flop_on.values())
+            # print(f"{int(bits, 2):x}")
+            # print(*(",".join(sorted(s)) for s in state.conjunction_memory.values()))
+            if state.rx_low:
+                return i
+            state = self.next_state(state)
+        raise AssertionError
 
     @classmethod
     def parse(cls, lines: Iterable[str]) -> Machine:
@@ -101,8 +135,8 @@ class Machine:
 
 def main():
     machine = Machine.parse(line.strip() for line in sys.stdin)
-    # print(machine.count_pulses(1))
-    print(machine.count_pulses(1000))
+    print(f"Part 1: {machine.count_pulses(1000)}")
+    print(f"Part 2: {machine.presses_until_rx_low()}")
 
 
 if __name__ == "__main__":
